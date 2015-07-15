63997f1c9cb251bd89ae40c21a63af75219fd7ad
Ivor Padilla <ivorjpc@gmail.com>
2015-07-15 10:31:14 -0430

    prettyfied address_book.rb with rubocop

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,37 +1,33 @@
 require_relative '../models/entry.rb'
 
 class AddressBook
-	attr_accessor :entries
+  attr_accessor :entries
 
-	def initialize
-		@entries = []
-	end
+  def initialize
+    @entries = []
+  end
 
-	def add_entry(name, phone, email)
-		index = 0
-		@entries.each do |entry|
-			if name < entry.name
-				break
-			end
-			index += 1
-		end
-		@entries.insert(index, Entry.new(name, phone, email))
-	end
+  def add_entry(name, phone, email)
+    index = 0
+    @entries.each do |entry|
+      break if name < entry.name
+      index += 1
+    end
+    @entries.insert(index, Entry.new(name, phone, email))
+  end
 
-	def remove_entry(entry_to_remove)
-	
-	new_arr = []
-		
-		@entries.each do |entry|
-			new_arr << entry.name
-			
-			if new_arr.include?(entry_to_remove)
-					i = new_arr.index(entry_to_remove)
-					@entries.delete_at(i)
-			else
-				new_arr
-			end		
-		end	
-	end
+  def remove_entry(entry_to_remove)
+    new_arr = []
+
+    @entries.each do |entry|
+      new_arr << entry.name
+
+      if new_arr.include?(entry_to_remove)
+        i = new_arr.index(entry_to_remove)
+        @entries.delete_at(i)
+      else
+        new_arr
+      end
+    end
+  end
 end
-
